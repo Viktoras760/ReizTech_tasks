2c88ef8ed7c73a84f0ec9db08b74cb9c46847704
Viktoras760 <35394736+Viktoras760@users.noreply.github.com>
2023-01-23 19:54:51 +0200

    Clock task finished

--- Task1/ClockTask/Program.cs
+++ Task1/ClockTask/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class ClockTask
+{
+    static void Main(string[] args)
+    {
+        int hour = GetHour();
+        int minutes = GetMinutes();
+        double angle = CalculateAngle(hour, minutes);
+        PrintAngle(angle);
+    }
+
+    static int GetHour()
+    {
+        int hour;
+        while (true)
+        {
+            Console.Write("Enter hour: ");
+            if (int.TryParse(Console.ReadLine(), out hour) && (hour >= 0 && hour <= 11))
+                break;
+            Console.WriteLine("Invalid input. Please enter a valid hour (0-11)");
+        }
+        return hour;
+    }
+
+    static int GetMinutes()
+    {
+        int minutes;
+        while (true)
+        {
+            Console.Write("Enter minutes: ");
+            if (int.TryParse(Console.ReadLine(), out minutes) && (minutes >= 0 && minutes <= 59))
+                break;
+            Console.WriteLine("Invalid input. Please enter a valid minute (0-59)");
+        }
+        return minutes;
+    }
+
+    static double CalculateAngle(int hour, int minutes)
+    {
+        double angle = Math.Abs((hour * 30 + (0.5 * minutes)) - (minutes * 6));
+        return Math.Min(angle, 360 - angle);
+    }
+
+    static void PrintAngle(double angle)
+    {
+        Console.WriteLine("Angle between hour and minute arrows: " + angle + " degrees");
+    }
+}
